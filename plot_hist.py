84a8853b702a4c91f396082ac926cf5ce72b748c
Antoine <antoine.eudes@gmail.com>
2019-04-08 09:33:25 +0200

    added chi2 test

--- plot_hist.py
+++ plot_hist.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from scipy.stats import norm
+from scipy import stats
 import numpy as np
 
 def plot_hist(fichier):
@@ -12,7 +12,10 @@ def plot_hist(fichier):
         value+=0.004
     data_crabe.close()
 
-    (mu, sigma) = norm.fit(X)
+    k2, p = stats.normaltest(X)
+    print("Test du Chi2 : Statistic=" + str(k2) + ", p-value=" + str(p))
+
+    (mu, sigma) = stats.norm.fit(X)
     x = mu + sigma * np.random.randn(10000)
     n, bins, patches = plt.hist(X, 29, density=True, facecolor='g')
     plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
