0bd59328c3570a6b7441de6fa4ec761644429c31
Unknown <alexperez@wanadoo.fr>
2019-04-13 15:09:58 +0200

    Cleaned chisquare.py

--- chisquare.py
+++ chisquare.py
@@ -7,7 +7,6 @@ from numpy.random import normal
 
 file = open('crabe.txt', 'r')
 
-# f_obs = []
 f_obs = np.empty(0)
 k=0
 
@@ -18,14 +17,8 @@ for line in file:
         f_obs = np.append(f_obs, 0.582+0.004*k)
     k += 1
     total += nb
-    # f_obs.append(int(line.strip('\n')))
 
-# f_obs = f_obs/np.sum(f_obs)
 mean, std = norm.fit(f_obs)
-print(mean)
-print(std)
-# X = np.linspace(0.582, 0.694, total)
-# f_gaussian = norm.pdf(X, mean, std)
 
 f_gaussian = normal(mean, std, len(f_obs))
 
@@ -33,12 +26,6 @@ f_gaussian = normal(mean, std, len(f_obs))
 print('\nObserved frequencies :\n{}\n'.format(f_obs))
 print('\nGaussian frequencies :\n{}\n'.format(f_gaussian))
 
-print(mean)
-print(std)
-
-# plt.plot(X, f_obs)
-# plt.plot(X, f_gaussian)
-# plt.show()
-
-
+print('mu : {}'.format(mean))
+print('std : {}'.format(std))
 print(chisquare(f_obs, f_gaussian))
