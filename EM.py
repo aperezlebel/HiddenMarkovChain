88b0263697ebde9f727873c27563f1c357a9e97d
Antoine <antoine.eudes@gmail.com>
2019-04-13 17:01:52 +0200

    Update EM.py

--- EM.py
+++ EM.py
@@ -1,29 +1,79 @@
-from math import sqrt, pi, exp
+import numpy as np
+from plot_hist import read_y
 
-def f(mu, sigma, yk):
-    return 1./sqrt(2*pi*sigma**2)*exp(-(yk-mu)**2/(2*sigma**2))
+def normale(y, m, s2):
+    return np.exp(-(y-m)**2/(2*s2))/np.sqrt(2*np.pi*s2)
 
-def EM(list_of_y_k):
-    # Initialisation
-    pi = [1./4, 3./4]
-    mu = [.57, .67]
-    sigma = [1./10000, 1./10000]
-    theta = [mu, sigma]
-    N_iter = 1000
-    N = len(list_of_y_k)
-    pi_aux = [0, 0]
+def f_theta(y, pi, mu, s2):
+    s = 0
+    for i in range(len(pi)):
+        s += pi[i]*normale(y, mu[i], s2[i])
+    return s
 
-    for _ in range(N_iter):
-        for j in range(2):
-            s = 0
-            for k in range(N):
-                rho_i_k = pi[j]*f(mu[j], sigma[j], list_of_y_k[k])/(pi[0]*f(mu[0], sigma[0], list_of_y_k[k])+pi[1]*f(mu[1], sigma[1], list_of_y_k[k]))
-                s += rho_i_k
-            pi_aux[j] = 1./N*s
-        pi = pi_aux[:]
-        
+def rho(y, i, pi, mu, s2):
+    return pi[i]*normale(y, mu[i], s2[i])/f_theta(y, pi, mu, s2)
 
-    return True
+def compute_pi_star(y, pi, mu, s2):
+    n, N = len(pi), len(y)
+    pi_star = np.zeros(n)
+    for i in range(n):
+        pi_star[i] = np.mean(rho(y, i=i, pi=pi, mu=mu, s2=s2))
 
-if __name__=='__main__':
-    print(f(0.5, 1, 1))
+    return pi_star
+
+def compute_mu_star(y, pi, mu, s2):
+    n, N = len(pi), len(y)
+    mu_star = np.zeros(n)
+    for i in range(n):
+        rho_y = rho(y, i, pi, mu, s2)
+        mu_star[i] = np.vdot(rho_y, y)/np.sum(rho_y)
+
+    return mu_star
+
+def compute_s2_star(y, pi, mu, s2, mu_star):
+    n, N = len(pi), len(y)
+    s2_star = np.zeros(n)
+    for i in range(n):
+        rho_y = rho(y, i, pi, mu, s2)
+        s2_star[i] = np.vdot(rho_y, (y-mu_star[i])**2)/np.sum(rho_y)
+
+    return s2_star
+
+def em(pi=[], mu=[], s2=[]):
+    if pi == [] or mu == [] or s2 == []:
+        pi=np.array([1./4, 3./4])
+        mu=np.array([.57, .67])
+        s2=np.array([1./10000, 1./10000])
+
+    y = read_y('crabe.txt')
+
+    print('\ny :\n{}'.format(y))
+    N = len(y)
+    N_iter=1000
+    theta=np.zeros((5, N+1))
+
+    print('\npi0 :\n{}'.format(pi))
+    print('mu0 :\n{}'.format(mu))
+    print('s20 :\n{}\n'.format(s2))
+
+    print('\nIteration (out of {}) :'.format(N_iter))
+    for k in range(N_iter):
+        pi_star = compute_pi_star(y, pi, mu, s2)
+        mu_star = compute_mu_star(y, pi, mu, s2)
+        s2_star = compute_s2_star(y, pi, mu, s2, mu_star)
+
+        pi = pi_star
+        mu = mu_star
+        s2 = s2_star
+
+        if (k+1) % (N_iter//10) == 0:
+            print('\t'+str(k+1))
+
+    print('\npi :\n{}'.format(pi_star))
+    print('mu :\n{}'.format(mu_star))
+    print('s2 :\n{}\n'.format(s2_star))
+
+    return pi, mu, s2
+
+if __name__ == '__main__':
+    em()
